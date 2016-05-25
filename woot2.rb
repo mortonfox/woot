d38c62a984c0653c708b4148485b203cf0518e5f
Morton Fox <github@qslw.com>
2016-05-25 00:19:00 -0400

    woot2: Add support for Woot-Off.

--- woot2.rb
+++ woot2.rb
@@ -7,21 +7,41 @@ require 'optparse'
 require 'ostruct'
 
 WOOT_URL = 'http://api.woot.com/2/events.json?key=95c1faa8df564b7cb65b681c8b07148d&eventType=Daily'.freeze
+WOOTOFF_URL = 'http://api.woot.com/2/events.json?key=95c1faa8df564b7cb65b681c8b07148d&eventType=WootOff'.freeze
 
-def show_woot
-  open(WOOT_URL) { |io|
+def offers url
+  open(url) { |io|
     results = JSON.parse(io.read)
-
-    results.sort_by { |res| res['Site'] }.each { |res|
-      domain = res['Site'].split('.').first
-      domain = 'woot' if domain == 'www'
-      offer = res['Offers'].first
-      soldout = offer['SoldOut'] ? 'Sold Out' : "#{format('%.0f', 100 - offer['PercentageRemaining'])}% sold"
-      price = '$' + format('%.2f', offer['Items'].first['SalePrice'])
-
-      puts format('%-11s %-46.46s %8s - %8s', domain, res['Title'], price, soldout)
-    }
+    Hash[
+      results.map { |res|
+        domain = res['Site'].split('.').first
+        domain = 'woot' if domain == 'www'
+        offer = res['Offers'].first
+        [domain, offer]
+      }
+    ]
   }
+end
+
+def show_woot domain, offer
+  soldout = offer['SoldOut'] ? 'Sold Out' : "#{format('%.0f', 100 - offer['PercentageRemaining'])}% sold"
+  price = '$' + format('%.2f', offer['Items'].first['SalePrice'])
+  puts format('%-11s %-46.46s %8s - %8s', domain, offer['Title'], price, soldout)
+end
+
+def show_woots
+  woot_list = offers WOOT_URL
+  woot_list.keys.sort.each { |domain|
+    show_woot domain, woot_list[domain]
+  }
+
+  wootoff_list = offers WOOTOFF_URL
+  unless wootoff_list.empty?
+    puts "\n=== Woot-Off! ==="
+    wootoff_list.keys.sort.each { |domain|
+      show_woot domain, wootoff_list[domain]
+    }
+  end
 end
 
 def parse_cmdline
@@ -62,14 +82,14 @@ if options.sleep
   begin
     loop do
       puts Time.now.strftime '%H:%M:%S'
-      show_woot
+      show_woots
       sleep options.sleep
     end
   rescue Interrupt
     puts 'Interrupted! Exiting...'
   end
 else
-  show_woot
+  show_woots
 end
 
 __END__
